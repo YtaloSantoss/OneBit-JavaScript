afa74938b0ecad5983386cde853e64c6ce7ac863
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2023-01-31 14:56:24 -0300

    atlz

---
+++ JavaScript-I/exercicio-1/recrutas.js
@@ -0,0 +1,4 @@
+const primeiroNome = prompt("Informe o primeiro nome do recruta:");
+const sobreNome = prompt("Informe o sobrenome do recruta:");
+const campoDeEstudo = prompt("Qual é o ano de nascimento do recruta?");
+const anoDeNascimento = prompt("Qual é o ano de nascimento do recruta?");
