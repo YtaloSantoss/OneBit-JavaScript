35f364274688ea8c3f4342e39df0e93751bdee9e
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2023-02-01 17:41:03 -0300

    atlz

---
+++ JavaScript-I/exercicio-4/index.js
@@ -0,0 +1,19 @@
+const turista = prompt("E ai, turista! Qual é o seu nome?")
+
+let cidades = " "
+let contagem =  0
+
+let continuar = prompt("Você visitou alguma cidade? (Sim/Não)")
+
+while (continuar === "Sim") {
+  let cidade = prompt("Qual é o nome da cidade visitada?")
+  cidades += " - " + cidade + "\n"
+  contagem++
+  continuar = prompt("Você visitou alguma outra cidade? (Sim/Não)")
+}
+
+alert(
+  "Turista: " + turista +
+  "\nQuantidade de cidades visitadas: " + contagem + 
+  "\nCidades visitadas: \n" + cidades
+)
