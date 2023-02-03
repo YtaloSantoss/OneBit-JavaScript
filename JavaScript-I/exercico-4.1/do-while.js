e2cea949e050ff98eaa835d9928465b008299c82
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2023-02-03 14:38:53 -0300

    atlz

---
+++ JavaScript-I/exercico-4.1/do-while.js
@@ -0,0 +1,9 @@
+let velocidade  = 20
+
+do {
+    alert("velocidade do veículo " + velocidade + "km/h")
+    velocidade -= 20
+} while (velocidade > 0)
+
+alert("Velocidade final: " + velocidade + " km/h") 
+alert("Velocidade parou") 
