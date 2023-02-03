4e1e4d382c8e28f862c6e7113e4d9183efc7abc3
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2023-02-03 15:11:55 -0300

    atlz

---
+++ JavaScript-I/exercicio-5/menu.js
@@ -0,0 +1,35 @@
+let opcao = ""
+
+do {
+  opcao = prompt (
+    "Seja bem-vindo(a)\n" +
+    "\nEscolha uma das opções abaixo:" +
+    "\n1. Opção Um" +
+    "\n2. Opção Dois" +
+    "\n3. Opção Três" +
+    "\n4. Opção Quatro" +
+    "\n5. Encerrar"
+  )
+
+switch (opcao){
+  case "1":
+    alert("Você escolheu a opção 1")
+    break
+  case "2":
+    alert("Você escolheu a opção 2")
+    break
+  case "3":
+    alert("Você escolheu a opção 3")
+    break
+  case "4":
+    alert("Você escolheu a opção 4")
+    break
+  case "5":
+    alert("Você escolheu a opção 5")
+    alert("Encerrando")
+    break
+  default: 
+    alert("Opção inválida")
+  }
+
+} while (opcao !== "5" )
