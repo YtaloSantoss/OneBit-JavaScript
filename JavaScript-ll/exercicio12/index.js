26a459ccf01faab34755402cbbf4b8a684f90eca
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2023-02-10 15:26:48 -0300

    atlz

---
+++ JavaScript-ll/exercicio12/index.js
@@ -0,0 +1,41 @@
+function createLabel(text, htmlFor) {
+  const label = document.createElement('label')
+  label.htmlFor = htmlFor
+  label.innerText = text
+  return label
+}
+
+function createInput(id, value, name, type = 'text', placeholder = ''){
+  const input = document.createElement('input')
+  input.id = id
+  input.value = value
+  input.name = name
+  input.type = type
+  input.placeholder = placeholder
+  return input
+}
+
+const addTechBtn = document.getElementById('addTechBtn')
+const form = document.getElementById('devForm')
+const developers = []
+let inputRows = 0
+
+addTechBtn.addEventListener('click', function (ev){
+  const stackInputs = document.getElementById('stackInputs')
+
+  const newRow = document.createElement('li')
+  const rowIndex = inputRows
+  inputRows++
+  newRow.id = 'inputRow-' + rowIndex
+  newRow.className = 'inputRow'
+
+  const techNameLabel = createLabel('Nome: ', 'techName-' + rowIndex)
+  const techNameInput = createInput('techName-' + rowIndex, null, 'techName')
+
+  newRow.append(
+    techNameLabel, techNameInput
+  )
+
+  stackInputs.appendChild(newRow)
+
+})
