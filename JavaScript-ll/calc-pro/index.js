710c61be743ef8ea173f10bf9fe90f3b8851bb88
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2023-02-13 19:36:33 -0300

    atlz

--- JavaScript-ll/calc-pro/index.js
+++ JavaScript-ll/calc-pro/index.js
@@ -6,8 +6,17 @@
  const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
 
-document.querySelectorAll('charkey')
+document.querySelectorAll('.charKey').forEach(function (charKeyBtn){
+  charKeyBtn.addEventListener('click', function (){
+    const value = charKeyBtn.dataset.value
+    input.value += value
+  })
+})
 
+  document.getElementById('clear').addEventListener('click', function (){
+    input.value = ''
+    input.focus()
+  })
 
 
  input.addEventListener('keydown', function (ev) {
@@ -24,6 +33,42 @@ document.querySelectorAll('charkey')
   }
 })
 
+document.getElementById('igual').addEventListener('click', calculate)
+
 function calculate() {
-  console.log('Calculando..')
+  resultInput.value = 'ERROR'
+  resultInput.classList.add('error')
+  const result = eval(input.value)
+  resultInput.value = result
+  resultInput.classList.remove('error')
 }
+
+document.getElementById('copyToClipboard').addEventListener('click', function (ev) {
+  const button = ev.currentTarget
+  if (button.innerText === "Copy") {
+    button.innerText = "Copied!"
+    button.classList.add("success")
+    navigator.clipboard.writeText(resultInput.value)
+  } else {
+    button.innerText = "Copy"
+    button.classList.remove("success")
+
+  }
+})
+
+
+document.getElementById('themeSwitcher').addEventListener("click", function () {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9")
+    root.style.setProperty("--border-color", "#aaa")
+    root.style.setProperty("--font-color", "#212529")
+    root.style.setProperty("--primary-color", "#26834a")
+    main.dataset.theme = "light"
+  } else {
+    root.style.setProperty("--bg-color", "#212529")
+    root.style.setProperty("--border-color", "#666")
+    root.style.setProperty("--font-color", "#f1f5f9")
+    root.style.setProperty("--primary-color", "#4dff91")
+    main.dataset.theme = "dark"
+  }
+})
