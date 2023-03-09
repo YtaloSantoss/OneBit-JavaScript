a9be94a36143ad9b059bbe94270aa83ea3e3a51f
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2023-03-09 19:21:44 -0300

    atlz

---
+++ JavaScript-V/23-requisicoes-GET/index.js
@@ -0,0 +1,42 @@
+function createCountryCard(country) {
+  const card = document.createElement('div')
+  card.classList.add('country')
+
+  const countryName = country.name.common//aqui foi pego dados pela API sobre nomes dos Países, caminho.
+  const name = document.createElement('h2')
+  name.textContent = countryName// no h2 que foi criado vai mostrar o nome do País.
+
+  const flag = document.createElement('img')
+  flag.src = country.flags.svg//aqui foi pego dados pela API sobre as images, caminho. 
+  flag.alt = countryName//um nome da img caso não consiga renderiza.
+
+  card.append(name, flag)
+  document.querySelector('#countries').append(card)//renderizando com html, sendo mostrado na tag main.
+}
+
+//...........................................................................
+
+
+async function getCountries() {
+  const response = await fetch('https://restcountries.com/v3.1/all') // requisicão GET
+  const countries = await response.json()
+
+  console.log(countries)
+  countries.forEach(createCountryCard)
+}
+
+getCountries()
+
+
+
+
+
+
+async function getCountries() {
+  const response = await fetch('https://restcountries.com/v3.1/all')
+  const countries = await response.json()
+
+  console.log(countries)
+}
+
+getCountries()
