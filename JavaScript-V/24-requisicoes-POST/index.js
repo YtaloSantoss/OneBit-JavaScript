76516a80181aeb7b62e7ba5c70af5f2a59b94139
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2023-03-10 15:22:50 -0300

    ATLZ

--- JavaScript-V/24-requisicoes-POST/index.js
+++ JavaScript-V/24-requisicoes-POST/index.js
@@ -1,1 +1,30 @@
- 
+function renderArticle(articleData) {
+  const article = document.createElement('article')
+  article.classList.add('article')
+  article.id = `article-${articleData.id}`
+
+  const title = document.createElement('h3')
+  title.classList.add('article-title')
+  title.textContent = articleData.title
+
+  const content = document.createElement('div')
+  content.classList.add('article-content')
+  content.innerHTML = articleData.content
+
+  const author = document.createElement('div')
+  author.classList.add('article-author')
+  author.textContent = articleData.author
+
+  article.append(title, author, content)
+  document.querySelector('#articles').appendChild(article)
+}
+
+//pegando os arquivos do backEnd, simulando pelo db.json  REQUISISÃO (GET)
+async function fetchArticles() {
+  const articles = await fetch("http://localhost:3000/articles").then(res => res.json())
+  articles.forEach(renderArticle)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetchArticles()
+})
