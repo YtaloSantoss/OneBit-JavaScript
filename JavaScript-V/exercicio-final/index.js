11d9bf051d44e55e4263daf6b72c72f96ba0bb4e
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2023-03-12 20:04:16 -0300

    atlz

--- JavaScript-V/exercicio-final/index.js
+++ JavaScript-V/exercicio-final/index.js
@@ -31,24 +31,106 @@ function createTransactionAmount(amount) {
 
 
   if (amount > 0) {// maoir que zero uma nova entrada quer dizer credito
-    span.textContent = `${formatedAmount} C`
-    span.classList.add('credit')
+    span.textContent = `${formatedAmount} Credito`  //positivo deposito 
+    span.classList.add('transaction-amount','credit')
   } else {// menor que zero é uma saida é um debit
-    span.textContent = `${formatedAmount} D`
-    span.classList.add('debit')
+    span.textContent = `${formatedAmount} Debito`//negativo debitado pagamento 
+    span.classList.add('transaction-amount','debit')
+
   }
   return span
 }
 
-//... 
+ 
+function createEditTransactionBtn(transaction) {
+  const editBtn = document.createElement('button')
+  editBtn.classList.add('edit-btn')
+  editBtn.textContent = 'Editar'
+  editBtn.addEventListener('click', () => {
+    document.querySelector('#id').value = transaction.id
+    document.querySelector('#name').value = transaction.name
+    document.querySelector('#amount').value = transaction.amount
+  })
+  return editBtn
+}
+
+
+
+function createDeleteTransactionButton(id) {
+  const deleteBtn = document.createElement('button')
+  deleteBtn.classList.add('delete-btn')
+  deleteBtn.textContent = 'Excluir'
+  deleteBtn.addEventListener('click', async () => {
+    await fetch(`http://localhost:3000/transactions/${id}`, { method: 'DELETE' })
+    deleteBtn.parentElement.remove()
+    const indexToRemove = transactions.findIndex((t) => t.id === id)
+    transactions.splice(indexToRemove, 1)
+    updateBalance()
+  })
+  return deleteBtn
+}
+
+
+
+
 function renderTransaction(transaction) {
   const container = createTransactionContainer(transaction.id)
   const title = createTransactionTitle(transaction.name)
   const amount = createTransactionAmount(transaction.amount)
- 
-  container.append(title, amount)
+  const editBtn = createEditTransactionBtn(transaction)
+  const deleteBtn = createDeleteTransactionButton(transaction.id)
+
+
+  container.append(title, amount, editBtn, deleteBtn)
   document.querySelector('#transactions').append(container)
 }
+
+
+async function saveTransaction(ev) {
+  ev.preventDefault()
+
+  const id = document.querySelector('#id').value 
+  const name = document.querySelector('#name').value
+  const amount = parseFloat(document.querySelector('#amount').value)
+
+  if (id) {
+    //editar a transação com id
+    const response = await fetch(`http://localhost:3000/transactions/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({ name, amount }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const transaction = await response.json()
+    const indexToRemove = transactions.findIndex((t) => t.id === id)
+    transactions.splice(indexToRemove, 1, transaction)
+    document.querySelector(`#transaction-${id}`).remove()
+    renderTransaction(transaction)
+  } else {  
+    //Criar nova transação
+    
+    const response = await fetch('http://localhost:3000/transactions' ,{
+      method: 'POST',
+      body: JSON.stringify({ name, amount }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const transaction = await response.json()
+    transactions.push(transaction)
+    renderTransaction(transaction)
+  }
+  
+
+  ev.target.reset()
+  updateBalance()
+}
+
+document.addEventListener('DOMContentLoaded', setup)
+document.querySelector('form').addEventListener('submit', saveTransaction)
+
+
 
 
 async function fetchTransactions() {
@@ -62,7 +144,7 @@ function updateBalance() {
   const formater = Intl.NumberFormat('pt-BR', {
     compactDisplay: 'long',
     currency: 'BRL',
-    style: 'currency'
+    style: "currency"
   })
   balanceSpan.textContent = formater.format(balance)
 }
