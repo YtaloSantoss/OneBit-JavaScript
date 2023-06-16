eba4392dfc4726ca95d9d6ae025b9655db67e1f2
Ytalo Mateus <99446046+YtaloSantoss@users.noreply.github.com>
2023-06-16 17:23:34 -0300

    atlz

---
+++ Praticando/1-exercicio/index.js
@@ -0,0 +1,9 @@
+alert("Bem vindo! Asseguir pediremos que informe alguns dados.")
+
+let name = prompt("Qual seu nome?")
+
+let age = prompt("Informe sua idade")
+
+let ageConfirmation = confirm("Sua idade é " + age + " anos")
+
+alert("Nome: " + name + "\nIdade: " + age + "\nIdade confirmada: " + ageConfirmation)
