63dfadb92159e13c6a552cdac8ae21d482e657cc
Ytalo Mateus <99446046+YtaloSantoss@users.noreply.github.com>
2023-06-18 15:30:53 -0300

    atlz

--- Praticando/5-exercicio/index.js
+++ Praticando/5-exercicio/index.js
@@ -10,4 +10,14 @@ if (confirmVelocity) {
   velocity = newVelocity
 }
 
-console.log(velocity)
+if (velocity <= 0) {
+  alert("Nave está parada. Considere partir e aumentar a velocidade")
+} else {
+  if (velocity < 40) {
+    alert("Você está devagar. Podemos aumentar mais.")
+  } else {
+    if (velocity < 80) {
+      alert("Parece uma boa velocidade para manter.")
+    }
+  }
+} 
