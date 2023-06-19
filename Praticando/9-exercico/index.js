bbfbaea784d7e0d08bc54c9a8f9f7d826ec53fc7
Ytalo Mateus <99446046+YtaloSantoss@users.noreply.github.com>
2023-06-19 17:26:29 -0300

    atlz

---
+++ Praticando/9-exercico/index.js
@@ -0,0 +1,14 @@
+let warpCount = 0
+let chosenOption = ""
+
+let spaceship = prompt("Digite o nome da nave")
+
+chosenOption = prompt("Deseja entrar em dobra espacial?\n1- Sim\n2- Não")
+
+
+while (chosenOption == "1") {
+  warpCount += 1
+  chosenOption = prompt("Deseja realizar a próxima dobra?\n1- Sim\n2- Não")
+}
+
+alert("Nave: " + spaceship + "\nQuatidade de dobras: " + warpCount)
