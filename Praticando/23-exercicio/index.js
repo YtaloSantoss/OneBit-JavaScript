c6ec3ba2105e4f812ce0fbccaab5d62b39df29b9
Ytalo Mateus <99446046+YtaloSantoss@users.noreply.github.com>
2023-06-21 17:11:03 -0300

    atlz

---
+++ Praticando/23-exercicio/index.js
@@ -0,0 +1,22 @@
+function saveHouse() {
+  let area = document.querySelector("input[name='area']").value
+  let number = document.querySelector("input[name='number']").value
+  let neighborhood = document.querySelector("input[name='neighborhood']").value
+  let city = document.querySelector("input[name='city']").value
+
+  let newListValue = document.createElement("li")
+  newListValue.innerText = area + "m², número " + number + " (" + neighborhood + " - " + city ")"
+
+  let removeButton = domcument.createElement("button")
+  removeButton.type = "button"
+  removeButton.innerText = "Remover"
+  removeButton.setAttribute("onclick", "removeHouse(this)")
+
+  newListValue.appendChild(removeButton)
+
+  document.getElementById("house-list").appendChild(newListValue)
+}
+
+function removeHouse(button) {
+
+}
